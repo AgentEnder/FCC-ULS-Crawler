5a74b9c429f856d742fbe3a8d6f41a46c64b3905
Craigory Coppola <craigory.coppola@universalplant.com>
2020-08-31 07:46:25 -0500

    Add project files.

---
+++ FCC-ULS-Crawler/Constants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCC_ULS_Crawler
+{
+    class SearchPageConstants
+    {
+        public const string searchBySelectCSS = "select[name=\"fiUlsSearchByType\"]";
+        public const string searchByFrnCSS = "option[value=\"uls_l_frn                     \"]";
+        public const string searchByValueCSS = "input[name=\"fiUlsSearchByValue\"]";
+        public const string searchBtnCSS = "input[alt=\"Search\"]";
+    }
+
+    class SearchResultsConstants
+    {
+        public const string licenseLinkCSS = "a[href*=\"license.jsp\"]";
+        public const string nextBtnCSS = "a[title*=\"Next page\"]";
+    }
+
+    class LicenseResultConstants
+    {
+
+    }
+}
---
+++ FCC-ULS-Crawler/Program.cs
@@ -0,0 +1,64 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FCC_ULS_Crawler
+{
+    class Program
+    {
+        const string baseUrl = "https://wireless2.fcc.gov/UlsApp/UlsSearch/";
+        const string licenseSearchURL = "searchLicense.jsp";
+        const string FRN = "0016251530";
+        static IWebDriver driver;
+        static void Main(string[] args)
+        {
+            // Setup chrome driver + navigate to ULS License Search
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(baseUrl + licenseSearchURL);
+
+            // Select search by FRN, enter FRN and click search.
+            driver.FindElement(By.CssSelector(SearchPageConstants.searchBySelectCSS)).Click();
+            driver.FindElement(By.CssSelector(SearchPageConstants.searchByFrnCSS)).Click();
+            var searchField = driver.FindElement(By.CssSelector(SearchPageConstants.searchByValueCSS));
+            searchField.Click();
+            searchField.SendKeys(FRN);
+            driver.FindElement(By.CssSelector(SearchPageConstants.searchBtnCSS)).Click();
+
+            // Get links to each license for given FRN, and iterate through them.
+            var licenseLinks = GetLicenseLinks();
+            Console.WriteLine($"{licenseLinks.Count()} licenses found!");
+            foreach (var licenseLink in licenseLinks)
+            {
+                driver.Navigate().GoToUrl(licenseLink);
+                Console.WriteLine(driver.Url);
+            }
+
+            driver.Close();
+            Console.ReadKey();
+        }
+
+        static List<string> GetLicenseLinks()
+        {
+            var licenseLinks = driver.FindElements(By.CssSelector(SearchResultsConstants.licenseLinkCSS)).Select(x => x.GetAttribute("href")).ToList();
+            while (true)
+            {
+                IWebElement nextButton;
+                try
+                {
+                    nextButton = driver.FindElement(By.CssSelector(SearchResultsConstants.nextBtnCSS));
+                }
+                catch (NoSuchElementException)
+                {
+                    break;
+                }
+                nextButton.Click();
+                licenseLinks.AddRange(
+                    driver.FindElements(By.CssSelector(SearchResultsConstants.licenseLinkCSS)).Select(x => x.GetAttribute("href")).ToList()
+                );
+            }
+            return licenseLinks;
+        }
+    }
+}
